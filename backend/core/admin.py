92e502beb3c3426b36100f798793dc5a73dd3ed7
LucasBotan <lucasmaiden@gmail.com>
2026-05-05 15:20:30 -0300

    Coluna quantidade_restante no Django Admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CentroDeTrabalho, Varejista
+from .models import CentroDeTrabalho, CodigoCliente, ItemNotaFiscal, NotaFiscal, Varejista
 
 
 @admin.register(CentroDeTrabalho)
@@ -17,3 +17,36 @@ class VarejistaAdmin(admin.ModelAdmin):
     search_fields = ['nome', 'cnpj', 'email', 'ct']
     readonly_fields = ['criado_em', 'atualizado_em']
     ordering = ['nome']
+
+
+@admin.register(CodigoCliente)
+class CodigoClienteAdmin(admin.ModelAdmin):
+    list_display = ['codigo', 'varejista', 'centro_de_trabalho', 'necessita_saida']
+    list_filter = ['necessita_saida', 'centro_de_trabalho']
+    search_fields = ['codigo', 'varejista__nome']
+    ordering = ['varejista', 'codigo']
+
+
+class ItemNotaFiscalInline(admin.TabularInline):
+    model = ItemNotaFiscal
+    extra = 0
+    fields = ['codigo_varejo', 'descricao', 'ean', 'quantidade', 'valor_unitario', 'gera_saida', 'status', 'quantidade_restante']
+    readonly_fields = ['gera_saida', 'quantidade_restante', 'status']
+
+
+@admin.register(NotaFiscal)
+class NotaFiscalAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'serie', 'varejista', 'centro_de_trabalho', 'data_emissao', 'valor_total', 'criado_em']
+    list_filter = ['centro_de_trabalho', 'varejista', 'data_emissao']
+    search_fields = ['numero', 'serie', 'chave_acesso', 'varejista__nome']
+    readonly_fields = ['criado_em', 'atualizado_em']
+    ordering = ['-criado_em']
+    inlines = [ItemNotaFiscalInline]
+
+
+@admin.register(ItemNotaFiscal)
+class ItemNotaFiscalAdmin(admin.ModelAdmin):
+    list_display = ['codigo_varejo', 'descricao', 'nota_fiscal', 'ean', 'ncm', 'quantidade', 'valor_unitario', 'quantidade_restante', 'gera_saida', 'status']
+    list_filter = ['status', 'gera_saida']
+    search_fields = ['codigo_varejo', 'descricao', 'ean']
+    ordering = ['nota_fiscal', 'codigo_varejo']
